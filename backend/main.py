c09c5763c9f75747497b352c0e60453f457c6161
Sakkoumhamza-usmba <hamza.sakkoum@usmba.ac.ma>
2026-01-24 11:26:17 +0100

    Ariel

--- backend/main.py
+++ backend/main.py
@@ -28,7 +28,7 @@ app = FastAPI(
 # CORS middleware for Next.js frontend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000","http://localhost:3002", "http://127.0.0.1:3002"],
+    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000","http://localhost:3001", "http://127.0.0.1:3001","http://localhost:3002", "http://127.0.0.1:3002"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
